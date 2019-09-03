0db35751020cede0812a7740ffddf7585b926e06
Shubham9123 <shubhamsehgal100@gmail.com>
2019-09-03 21:17:55 +0530

    Add files via upload

--- wallpapers.bat
+++ wallpapers.bat
@@ -1,5 +1,9 @@
 @echo off
+echo Any existing directory named MSWallpaper will be deleted? Are you sure you want to continue?
+pause
 echo Copying wallpapers from %userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+echo Deleting MSWallpaper folder on desktop
+del "%userprofile%\Desktop\MSWallpapers"
 echo Creating Directory MSWallpapers. All images will be in it.
 mkdir "%userprofile%\Desktop\MSWallpapers"
 echo Copying Files from %userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
@@ -7,6 +11,8 @@ copy "%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryMana
 echo Converting extension to jpg
 ren "%userprofile%\Desktop\MSWallpapers\*.*" "*.jpg"
 echo Success! Checkout MSWallpapers directory on desktop.
+echo opening folder
+explorer "%userprofile%\Desktop\MSWallpapers"
 pause
 
 
